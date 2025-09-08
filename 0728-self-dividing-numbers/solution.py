b6036f79a9c4f482dac41755f41ed89f2684a9c7
Darsana Kumpatla <Darsanakumpatla46@gmail.com>
2025-09-08 03:58:04 +0000

    Sync LeetCode submission Runtime - 4 ms (74.82%), Memory - 17.9 MB (100.00%)

---
+++ 0728-self-dividing-numbers/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> list[int]:
+        def is_self_dividing(num: int) -> bool:
+            original = num
+            while num > 0:
+                digit = num % 10
+                if digit == 0 or original % digit != 0:
+                    return False
+                num //= 10
+            return True
+
+        return [num for num in range(left, right + 1) if is_self_dividing(num)]
