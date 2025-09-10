eeddc401e24dadfa3445f615699983360bade93a
Darsana Kumpatla <Darsanakumpatla46@gmail.com>
2025-09-10 10:11:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.7 MB (100.00%)

---
+++ 0268-missing-number/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        S=sum(nums)
+        missing_num=n*(n+1)//2-S
+        return missing_num
+        
