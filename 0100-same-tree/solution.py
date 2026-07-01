b1355352659d2b2c3525e1401a2224c4cc11cb58
Darsana Kumpatla <Darsanakumpatla46@gmail.com>
2026-07-01 06:37:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (94.35%)

--- 0100-same-tree/solution.py
+++ 0100-same-tree/solution.py
@@ -1,23 +1,15 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):def __init__(self, val=0, left=None, right=None):
+#     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # If both trees are empty, they are the same
         if not p and not q:
             return True
-        
-        # If one tree is empty and the other is not, they are not the same
-        if not p or not q:
+        if (p and not q)or (q and not p):
             return False
-        
-        # If the values of the nodes are different, the trees are not the same
-        if p.val != q.val:
+        if p.val!=q.val:
             return False
-        
-        # Recursively check the left and right subtrees
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return self.isSameTree(p.left,q.left)and self.isSameTree(p.right,q.right)
